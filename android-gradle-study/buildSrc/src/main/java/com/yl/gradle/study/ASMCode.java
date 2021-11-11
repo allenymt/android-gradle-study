0a559e51cdfb556c132b261d5a0993fd2f857e72
yulun <yulun@weidian.com>
2021-11-11 20:58:15 +0800

    加点注释

--- android-gradle-study/buildSrc/src/main/java/com/yl/gradle/study/ASMCode.java
+++ android-gradle-study/buildSrc/src/main/java/com/yl/gradle/study/ASMCode.java
@@ -110,36 +110,37 @@ public class ASMCode {
 
         @Override
         protected void onMethodEnter() {
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
-            timeLocalIndex = newLocal(Type.LONG_TYPE); //这个是LocalVariablesSorter 提供的功能，可以尽量复用以前的局部变量
-            mv.visitVarInsn(LSTORE, timeLocalIndex);
+            //method enter
+            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false); // 调用类的静态方法
+            timeLocalIndex = newLocal(Type.LONG_TYPE); //new 一个局部变量
+            mv.visitVarInsn(LSTORE, timeLocalIndex);// 把第一步的结果(在栈顶)，放到局部变量中，timeLocalIndex就是在局部变量表中的位置
         }
 
         @Override
         protected void onMethodExit(int opcode) {
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
-            mv.visitVarInsn(LLOAD, timeLocalIndex);
-            mv.visitInsn(LSUB);//此处的值在栈顶
+            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false); // 同第一步
+            mv.visitVarInsn(LLOAD, timeLocalIndex); // 加载第一个大步存储的值
+            mv.visitInsn(LSUB);//此处的值在栈顶,做减法
             mv.visitVarInsn(LSTORE, timeLocalIndex);//因为后面要用到这个值所以先将其保存到本地变量表中
 
 
-            int stringBuilderIndex = newLocal(Type.getType("Ljava/lang/StringBuilder"));
-            mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder");
-            mv.visitInsn(Opcodes.DUP);
-            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
+            int stringBuilderIndex = newLocal(Type.getType("Ljava/lang/StringBuilder")); // new一个局部变量
+            mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder"); //new 一个实例
+            mv.visitInsn(Opcodes.DUP);// 复制栈顶实例
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false); //执行实例的初始化
             mv.visitVarInsn(Opcodes.ASTORE, stringBuilderIndex);//需要将栈顶的 stringbuilder 保存起来否则后面找不到了
-            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex);
-            mv.visitLdcInsn(className + "." + methodName + " time:");
-            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
-            mv.visitInsn(Opcodes.POP);
-            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex);
-            mv.visitVarInsn(Opcodes.LLOAD, timeLocalIndex);
-            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
-            mv.visitInsn(Opcodes.POP);
-            mv.visitLdcInsn("Geek");
-            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex);
-            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
-            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I", false);//注意： Log.d 方法是有返回值的，需要 pop 出去
+            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex); // 加载局部 变量
+            mv.visitLdcInsn(className + "." + methodName + " time:"); //常量值，此时这个常量在栈顶
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false); //执行方法
+            mv.visitInsn(Opcodes.POP); // 出栈，情况栈
+            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex); // 入栈sb对象，也就是append方法的第一个参数
+            mv.visitVarInsn(Opcodes.LLOAD, timeLocalIndex); // 入栈时间的差值
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false); // 执行方法
+            mv.visitInsn(Opcodes.POP); //保持栈的空状态
+            mv.visitLdcInsn("Geek"); //加载常量
+            mv.visitVarInsn(Opcodes.ALOAD, stringBuilderIndex); // 入参sb
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false); //sb的toString
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I", false);//第一个参数就是常量Geek,第二个参数是sb的ToString(),注意： Log.d 方法是有返回值的，需要 pop 出去
             mv.visitInsn(Opcodes.POP);//插入字节码后要保证栈的清洁，不影响原来的逻辑，否则就会产生异常，也会对其他框架处理字节码造成影响
         }
     }
